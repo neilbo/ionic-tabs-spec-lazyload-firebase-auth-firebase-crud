15d6c7ae309867889f4f8f2defbbd72e255ef2eb
Neil Estandarte <NEstandarte@lgiasuper.com.au>
2017-11-28 14:15:05 +1000

    Setup and Add Unit Test

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,44 @@
+
+import { async, TestBed } from '@angular/core/testing';
+import { IonicModule } from 'ionic-angular';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule} from '@angular/http';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { MyApp } from './app.component';
+
+describe('MyApp Component', () => {
+  let fixture;
+  let component;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+          MyApp
+          ],
+      imports: [
+        BrowserModule,
+        HttpModule,
+        IonicModule.forRoot(MyApp)
+      ],
+      providers: [
+        StatusBar,
+        SplashScreen
+      ]
+    })
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MyApp);
+    component = fixture.componentInstance;
+  });
+
+  it ('should be created', () => {
+    expect(component instanceof MyApp).toBe(true);
+  });
+  
+  it('should set rootPage to TabsPage', () => {
+    expect(component.rootPage).toBe('TabsPage');
+  });
+});
